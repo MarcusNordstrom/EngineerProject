0139c22deb3656b5f7ab731a37ea4d4747bbd7e9
hfr <hfr@hybridpro.dk>
2019-10-18 13:07:25 +0200

    Added functionality to get word from specific node

--- php-scripts/nodedisplay.php
+++ php-scripts/nodedisplay.php
@@ -5,7 +5,7 @@
   <?php 
   require_once 'displaycontroller.php';
   
-  $word = GetWord();
+  $word = getWordFromId(10);
  
   $wordArray = str_split($word[0]);
   $deadLetters = str_split($word[1]);?>
